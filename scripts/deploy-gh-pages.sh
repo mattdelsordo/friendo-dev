04cc0c64f96d52ea21391a89f22b6d6453418d58
Matt DelSordo <mattdelsordo@gmail.com>
2018-08-29 00:02:06 -0400

    :green_heart: Merge CI changes from master

--- scripts/deploy-gh-pages.sh
+++ scripts/deploy-gh-pages.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-# build webpage
-yarn build-prod
-git add -A
-git commit -m ":rocket:"
+# check to see if the last build was a build commit, don't build if so
+if [[ $(git log -1 --pretty=%B) != ":rocket:" ]]; then
+    # build webpage
+    git checkout master
+    yarn build-prod
+    git add -A
+    git commit -m ":rocket:"
 
-# push to repository
-PUSH_URI="https://${GITHUB_TOKEN}@github.com/mattdelsordo/friendo-dev.git"
-echo "Pushing to master"
-git push "${PUSH_URI}" >/dev/null 2>&1 # don't print secrets
+    # push to repository
+    PUSH_URI="https://${GITHUB_TOKEN}@github.com/mattdelsordo/friendo-dev.git"
+    echo "Pushing to master"
+    git push "${PUSH_URI}" >/dev/null 2>&1 # don't print secrets
+fi
