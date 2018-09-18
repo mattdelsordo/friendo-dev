ce338e7883b3370bb659f54743f94b89c155ee82
Matt DelSordo <mattdelsordo@gmail.com>
2018-09-17 23:41:09 -0400

    :sparkles: Add dog training state infrastructure

---
+++ src/friendo/state/fitness/dog-cuddle.js
@@ -0,0 +1,75 @@
+import Exercise from './exercise'
+import { left, right } from '../../../art/art-util'
+import { STATS } from '../../constants'
+
+export const ID = `state_${STATS.DOG}`
+
+export default class DogCuddle extends Exercise {
+  constructor(savedState) {
+    super(savedState)
+    this.id = ID
+  }
+
+  draw(g, x, y, friendo) {
+    super.draw(g, x, y, friendo)
+
+    // decide which frame shall be displayed
+    this.frame = (this.frame + 1) % 4
+    switch (this.frame) {
+      case 2:
+        this.frame2(g, x, y, friendo)
+        break
+      case 3:
+        this.frame2(g, x, y, friendo)
+        break
+      case 1:
+        this.frame1(g, x, y, friendo)
+        break
+      case 0:
+      default:
+        this.frame1(g, x, y, friendo)
+        break
+    }
+  }
+
+  frame1(g, x, y, friendo) {
+    // pre-compute constants for drawing for ease of readability
+    const { thighGap, bodyOffset } = friendo.element
+    const legBrush = friendo.element.legBrush(friendo)
+    const armBrush = friendo.element.armBrush(friendo)
+    const armOffset = {
+      x: friendo.element.armOffset.xOffset,
+      y: friendo.element.legHeight - friendo.element.armOffset.yOffset,
+    }
+    const armAngle = 0.25 // pi radians
+
+    left(g, x - thighGap, y, legBrush) // left leg
+    right(g, x + thighGap, y, legBrush) // right leg
+    left(g, x - armOffset.x, y - armOffset.y, armBrush, armAngle)// left arm
+    right(g, x + armOffset.x, y - armOffset.y, armBrush, armAngle)// right arm
+    const computedTethers = friendo.element.drawCore(g, x, y - bodyOffset, friendo, this.blink)
+    friendo.element.speak(g, x + computedTethers.speech.x, computedTethers.speech.y, friendo)
+    return computedTethers
+  }
+
+  frame2(g, x, y, friendo) {
+    // pre-compute constants for drawing for ease of readability
+    const { thighGap } = friendo.element
+    const bodyOffset = friendo.element.bodyOffset - (friendo.element.bodyOffset * 0.05)
+    const legBrush = friendo.element.legBrush(friendo)
+    const armBrush = friendo.element.armBrush(friendo)
+    const armOffset = {
+      x: friendo.element.armOffset.xOffset,
+      y: friendo.element.legHeight - friendo.element.armOffset.yOffset,
+    }
+    const armAngle = 0.30 // pi radians
+
+    left(g, x - thighGap, y, legBrush) // left leg
+    right(g, x + thighGap, y, legBrush) // right leg
+    left(g, x - armOffset.x, y - armOffset.y, armBrush, armAngle)// left arm
+    right(g, x + armOffset.x, y - armOffset.y, armBrush, armAngle)// right arm
+    const computedTethers = friendo.element.drawCore(g, x, y - bodyOffset, friendo, this.blink)
+    friendo.element.speak(g, x + computedTethers.speech.x, computedTethers.speech.y, friendo)
+    return computedTethers
+  }
+}
