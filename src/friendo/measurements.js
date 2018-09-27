908446265e12553bfafe70860a715d35fded539b
Matt DelSordo <mattdelsordo@gmail.com>
2018-09-27 13:55:06 -0400

    :recycle: Modify leg and arm scaling to use raw stats

--- src/friendo/measurements.js
+++ src/friendo/measurements.js
@@ -5,9 +5,9 @@ import { STATS } from './constants'
  */
 
 export const thighGap = () => 13
-export const armGirth = friendo => friendo.getStatStage(STATS.ARM) * 2
-export const armLength = friendo => Math.floor(((friendo.getStatStage(STATS.ARM) - 1) * 6) + 10)
-export const legGirth = friendo => friendo.getStatStage(STATS.LEG) * 2
-export const legHeight = friendo => ((friendo.getStatStage(STATS.LEG) - 1) * 6) + 10
+export const armGirth = friendo => (friendo.getStat(STATS.ARM) * 0.2) + 2
+export const armLength = friendo => Math.floor(((friendo.getStat(STATS.ARM) - 1) * 0.6) + 10)
+export const legGirth = friendo => (friendo.getStat(STATS.LEG) * 0.2) + 2
+export const legHeight = friendo => ((friendo.getStat(STATS.LEG) - 1) * 0.6) + 10
 export const footLength = friendo => Math.floor(legGirth(friendo) * 1.3) + 3
 export const footHeight = friendo => legHeight(friendo) / 4
