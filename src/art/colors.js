fa84a412050a7d3267bb06bda7555e72fe2555f6
Matt DelSordo <mattdelsordo@gmail.com>
2018-12-27 16:58:58 -0500

    :art: Add egg-state colors and update others to hexadecimal

--- src/art/colors.js
+++ src/art/colors.js
@@ -1,17 +1,35 @@
+/**
+ * Frequently used reference colors.
+ * https://encycolorpedia.com/ - Color reference
+ * http://paletton.com/ - Color pallets
+ */
+
 // element colors
-export const EARTH_OUTLINE = 'rgb(79, 57, 36)'
-export const EARTH_SKIN = 'rgb(166, 145, 123)'
-export const WATER_OUTLINE = 'rgb(24, 73, 129)'
-export const WATER_SKIN = 'rgb(137, 181, 231)'
-export const AIR_OUTLINE = 'rgb(86, 190, 186)'
-export const AIR_SKIN = 'rgb(225, 242, 249)'
-export const FIRE_OUTLINE = 'rgb(255, 0, 0)'
-export const FIRE_SKIN = 'rgb(255, 206, 0)'
-export const DEFAULT_SKIN = 'white'
-export const DEFAULT_OUTLINE = 'black'
+export const EARTH_OUTLINE = '#4f3924'
+export const EARTH_SKIN = '#a6917b'
+export const WATER_OUTLINE = '#184981'
+export const WATER_SKIN = '#89b5e7'
+export const AIR_OUTLINE = '#56beba'
+export const AIR_SKIN = '#e1f2f9'
+export const FIRE_OUTLINE = '#ff0000'
+export const FIRE_SKIN = '#ffce00'
+export const DEFAULT_SKIN = '#ffffff'
+export const DEFAULT_OUTLINE = '#000000'
 
 // dog colors
-export const DOG_SKIN = 'rgb(229, 205, 108)'
-export const DOG_OUTLINE = 'rgb(140, 98, 57)'
-export const TONGUE_FILL = 'pink'
-export const TONGUE_LINE = 'deeppink'
+export const DOG_SKIN = '#e5cd6c'
+export const DOG_OUTLINE = '#8c6239'
+export const TONGUE_FILL = '#ffc0cb'
+export const TONGUE_LINE = '#ff1493'
+
+// egg colors
+export const EARTH_EGG_OUTLINE = '#63472d'
+export const EARTH_EGG_SKIN = '#c3b5a6'
+export const WATER_EGG_OUTLINE = '#1e5ba1'
+export const WATER_EGG_SKIN = '#d5e5f7'
+export const AIR_EGG_OUTLINE = '#88d1ce'
+export const AIR_EGG_SKIN = '#ffffff'
+export const FIRE_EGG_OUTLINE = '#ff4040'
+export const FIRE_EGG_SKIN = '#ffda40'
+export const DEFAULT_EGG_SKIN = '#ffffff'
+export const DEFAULT_EGG_OUTLINE = '#080808'
