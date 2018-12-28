63afb058d4cd8e55b685dbb8a578fb1759235332
Matt DelSordo <mattdelsordo@gmail.com>
2018-12-28 16:14:39 -0500

    :sparkles: Add shake effect to egg state

--- src/friendo/state/egg.js
+++ src/friendo/state/egg.js
@@ -4,6 +4,7 @@
  */
 
 import State from './state'
+import { STATS } from '../constants'
 
 export const ID = 'state_egg'
 
@@ -21,10 +22,27 @@ export default class Egg extends State {
   draw(g, x, y, friendo) {
     super.draw(g, x, y, friendo)
 
+    // Shake if not currently shaking and with a probability
+    // based on egg level
+    const rand = Math.floor(Math.random() * 100)
+    const shake = friendo._h_stats[STATS.EGG] >= rand
+    if (!this.shaking && shake) {
+      this.shaking = true
+    }
+
+    // do a shake
+    if (this.shaking) {
+      this.shaking = false
+      return this.frame2(g, x, y, friendo)
+    }
     return this.frame1(g, x, y, friendo)
   }
 
   frame1(g, x, y, friendo) {
     friendo.element.drawEgg(g, x, y, friendo)
+  }
+
+  frame2(g, x, y, friendo) {
+    friendo.element.drawEgg(g, x + 2, y, friendo)
   }
 }
