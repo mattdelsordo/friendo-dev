5b24f14b3e8fbf45f0f51699caa8cc20f828f7b5
Matt DelSordo <mattdelsordo@gmail.com>
2019-01-28 23:50:24 -0500

    :green_heart: Only perform vernum test if pr build

--- scripts/test-vernum.sh
+++ scripts/test-vernum.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-#only perform check if in the develop branch
-if [[ $TRAVIS_BRANCH == "master" ]]; then
+# only perform check if build is
+# 1. a travis build
+# 2. a pull request build
+# 3. merging into master
+if [[ $TRAVIS_PULL_REQUEST_SLUG != "" && $TRAVIS_BRANCH == "master" ]]; then
     # If the package.json version matches the git tag, fail
     GIT_VER=$(git describe --abbrev=0 | egrep -o '[0-9]+\.[0-9]+\.[0-9]+')
     JSON_VER=$(egrep '"version": "([0-9]+\.[0-9]+\.[0-9]+"),' package.json | egrep -o '[0-9]+\.[0-9]+\.[0-9]+')
