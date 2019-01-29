1f30b7af45c4bcee60a1b3d9e9e9793278a372b6
Matt DelSordo <mattdelsordo@gmail.com>
2019-01-28 23:02:20 -0500

    :green_heart: Tag master build with new version number

--- scripts/deploy-gh-pages.sh
+++ scripts/deploy-gh-pages.sh
@@ -6,10 +6,15 @@ if [[ $(git log --oneline -n1) != *":rocket:"* ]]; then
     git checkout master
     yarn build-prod
     git add -A
-    git commit -m ":rocket:"
+
+    TAG=$(egrep '"version": "([0-9]+\.[0-9]+\.[0-9]+"),' package.json | egrep -o '[0-9]+\.[0-9]+\.[0-9]+')
+
+    git commit -m ":rocket: v$TAG"
+    git tag -a "v$TAG" -m "v$TAG"
 
     # push to repository
     PUSH_URI="https://${GITHUB_TOKEN}@github.com/mattdelsordo/friendo.git"
     echo "Pushing to master"
     git push "${PUSH_URI}" >/dev/null 2>&1 # don't print secrets
+    git push "${PUSH_URI}" --tags  >/dev/null 2>&1
 fi
