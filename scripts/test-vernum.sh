b0afbb5bad3605a4b1d8eed19f884dd420ceb991
Matt DelSordo <mattdelsordo@gmail.com>
2019-01-28 23:26:59 -0500

    :green_heart: Try to fix version number check

--- scripts/test-vernum.sh
+++ scripts/test-vernum.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #only perform check if in the develop branch
-if [[ $(git branch | grep \* | cut -d ' ' -f2) == "develop" ]]; then
+if [[ $TRAVIS_BRANCH == "master" ]]; then
     # If the package.json version matches the git tag, fail
     GIT_VER=$(git describe --abbrev=0 | egrep -o '[0-9]+\.[0-9]+\.[0-9]+')
     JSON_VER=$(egrep '"version": "([0-9]+\.[0-9]+\.[0-9]+"),' package.json | egrep -o '[0-9]+\.[0-9]+\.[0-9]+')
