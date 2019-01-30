a1af31518c7ded4877fbf89e8e40bc3d76dc223c
Matt DelSordo <mattdelsordo@gmail.com>
2019-01-29 19:05:38 -0500

    :lipstick: Link footer version num to release notes

--- src/controller/footer.js
+++ src/controller/footer.js
@@ -6,7 +6,7 @@ import $ from 'jquery'
 import { version } from '../../package.json'
 
 export default () => {
-  $('#vernum').html(`v${version}`)
+  $('#vernum').html(`[ v${version} ]`).attr('href', `https://github.com/mattdelsordo/friendo/releases/tag/v${version}`)
 
   $('#game-info-icon').mouseenter(() => {
     $('#game-info').css('visibility', 'visible')
