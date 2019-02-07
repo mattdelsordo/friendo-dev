e6fa95af9ee8ba93d12836056ca6a1131897e3a0
Matt DelSordo <mattdelsordo@gmail.com>
2019-02-07 15:57:58 -0500

    :sparkles: Add delete button with flavor text

--- src/game/setup/header-listeners.js
+++ src/game/setup/header-listeners.js
@@ -5,6 +5,20 @@
 import $ from 'jquery'
 import { version } from '../../../package.json'
 
+const deleteFriendo = (friendo) => {
+  console.log('called')
+  if (!friendo) {
+    alert('No Friendo DNA found!')
+  } else if ($('#delete-check1').prop('checked') && $('#delete-check2').prop('checked')) {
+    // handle deletion flavor text
+    if (friendo.level >= 1) {
+      alert(`"... ${friendo.owner}... goodbye..."`)
+    } else {
+      alert('You hear a cracking noise as the egg fades into nothingness.')
+    }
+  }
+}
+
 export default () => {
   $('#vernum').html(`[ v${version} ]`).attr('href', `https://github.com/mattdelsordo/friendo/releases/tag/v${version}`)
 
@@ -14,5 +28,15 @@ export default () => {
 
   $('#header').mouseleave(() => {
     $('#game-info').css('visibility', 'hidden')
+  })
+
+  $('#delete-btn').click(() => {
+    deleteFriendo()
+  })
+}
+
+export const updateDelete = (friendo) => {
+  $('#delete-btn').off('click').click(() => {
+    deleteFriendo(friendo)
   })
 }
