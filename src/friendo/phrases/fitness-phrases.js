ba0090d03412c6c22af7d0dc6e16b10c1a9a7bfc
Matt DelSordo <mattdelsordo@gmail.com>
2019-10-12 20:45:52 -0400

    add some fitness phrases

--- src/friendo/phrases/fitness-phrases.js
+++ src/friendo/phrases/fitness-phrases.js
@@ -4,10 +4,11 @@ export default () => [
   'hef',
   'huff',
   'urkk',
-  'p\'shoo',
   'feel the burn',
   'aw yeah',
   'pump it',
   'live for the grind',
   'hoo boy',
+  'hup two three four',
+  'fight! on! fight! on!',
 ]
